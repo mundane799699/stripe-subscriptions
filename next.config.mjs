bfb8874ee03cd9d304d6475892ed85cdfd7f06e1
mundane <799699348@qq.com>
2025-01-20 21:56:51 +0800

    改一下试试

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Credentials",
+            value: "true",
+          },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://stripe-subscriptions-jet.vercel.app",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
